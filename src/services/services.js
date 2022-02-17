4a4cf1557f46e0237a690f5dd219d8def01e82dc
Felipe Sakuma <fycorsak@gmail.com>
2022-02-17 14:53:53 -0300

    chore: change env variable

--- src/services/services.js
+++ src/services/services.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 function createConfig(token) {
   return { headers: { Authorization: `Bearer ${token}` } };
